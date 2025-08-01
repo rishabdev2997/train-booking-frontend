037795e5a0ec5942db8557769c67d62f9d26777c
Rishabdev <rishabofficial2997@gmail.com>
2025-08-02 00:02:18 +0530

    env file

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
